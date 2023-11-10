3751053f5a79afdbbb78fb4d2ef15faf59d136aa
Mary P <marylaboitebis@gmail.com>
2023-11-10 11:01:56 +0100

    mise en place des profils des photographes dans le fichier photographer.js, avec ajout des images dnas assets

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,39 +1,14 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data Paris",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Ma data Londres ",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Ma data NewYork",
-                "id": 3,
-                "city": "New York",
-                "country": "USA",
-                "tagline": "Ceci est ma data test 3",
-                "price": 350,
-                "portrait": "account.png"
-            },
-        ]
+     // requête sur le fichier JSON en utilisant "fetch".
+        const response = await fetch("./../../data/photographers.json");
+        const photographers = await response.json();
+        console.log(photographers);
+   
         // et bien retourner le tableau photographers seulement une fois récupéré
         // ici on récupéres 3x le tableau photographers. 
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        // return ({
+        //     photographers: [...photographers]});
+        return photographers;
     }
 
     async function displayData(photographers) {
@@ -42,7 +17,7 @@
         photographers.forEach((photographer) => {
             //Récupère les données d'un seul photographe du tableau photographers.
             const photographerModel = photographerTemplate(photographer);
-            console.log(photographerModel);
+            // console.log(photographerModel);
             //Ici on va récupérer l'article dans son ensemble, créé grâce à la fonction getUserCardDOM
             const userCardDOM = photographerModel.getUserCardDOM();
             // ici on va insérer dans les données de la const userCardDOM dans la section .photographer_section.
@@ -55,6 +30,7 @@
         const { photographers } = await getPhotographers();
         //affiche les données du tableau photographers;
         displayData(photographers);
+
     }
     
     init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,23 +1,53 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
+    console.log (name, portrait, id, city, country, tagline, price);
     // Récupère l'image en fonction de la donnée 'portrait' du tableau JSON récupéré via data
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `${portrait}`;
     // fonction qui permet la mise en page de la card du photographe
+
     function getUserCardDOM() {
         // On crée un élément article
         const article = document.createElement( 'article' );
+        article.classList.add("cardUser");
+
           // On crée un élément image
         const img = document.createElement( 'img' );
-        // On lui met la src qui provient de la constante picture car c'est la même chose
-        img.setAttribute("src", picture)
-        // On crée un titre
+        img.setAttribute("src", picture); // On lui met la src qui provient de la constante picture
+        img.classList.add("cardUser__img");
+
+        // On crée le titre avec le nom
         const h2 = document.createElement( 'h2' );
-        // Dans lequel on insère le name de la const data
-        h2.textContent = name;
+        h2.textContent = `${name}`;
+        h2.classList.add("cardUser__title");
+
+        // Création du paragraphe de la ville et du pays.
+        const cityCountry = document.createElement('p');
+        cityCountry.innerText = `${city}, ${country}`;
+        cityCountry.classList.add("cardUser__cityCountry");
+
+        // Création du paragraphe de la tagline
+        const taglineText = document.createElement('p');
+        taglineText.innerText =  tagline;
+        taglineText.classList.add("cardUser__tagline");
+
+        // Création du paragraphe de la tagline
+        const priceData = document.createElement('p');
+        priceData.innerText =  `${price} € / jour`;
+        priceData.classList.add("cardUser__tagline");
+
+
+
         //on ajoute un enfant img à article 
         article.appendChild(img);
         // Puis on ajoute le h2
         article.appendChild(h2);
+        // ajout de la ville et du pays
+        article.appendChild(cityCountry);
+        // ajout de la tagline
+        article.appendChild(taglineText);
+        // ajout du prix
+        article.appendChild(priceData);
+
         // et évidemment, on doit retourner l'article
         return (article);
     }
